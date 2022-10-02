8786c18fd580abecaa96e1f5a7bbe2d48e7f8875
Oksanka25 <chekoksa25@gmail.com>
2022-10-02 17:38:19 -0400

    ready for heroku deploy attempt #1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Starting an app
 const express = require("express");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // 2. Requiring and Initializing
 require('./config/db.connection');
