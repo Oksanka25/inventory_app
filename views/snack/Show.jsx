609dd5570ec7462965e4807015d7aad30eff1bf2
Oksanka25 <chekoksa25@gmail.com>
2022-10-01 20:49:12 -0400

    Working on a Show page

--- views/snack/Show.jsx
+++ views/snack/Show.jsx
@@ -40,42 +40,60 @@ class Show extends React.Component {
                     <a href="/snack"> Back to the List of Snacks</a>
 
 
-                    <section>
-                        <h3>Add a Review:</h3>
-                        <form method="POST" action="/feedback">
-                            <label for="comment"> Comment: </label>
-                            <input id="comment" name="comment" type="textarea" required />
-                            <br />
-                            <label for="rating"> Rating: </label>
-                            <input id="rating" name="rating" type="number" min="0" step="1" max="5" required />
-                            <br />
-                            <label for="snackItem"> Item: </label>
-                            <input type="snackItem" name="snackItem" value={oneSnackItem._id} required /> <br />
-                            <input type="submit" value="Add this Item..." />
-                        </form>
-                    </section>
-                    <section>
-                        <h2>Reviews of {oneSnackItem.name}</h2>
-                        {feedbacks.map((fb, i) => {
-                            return <>
-                                <p> Comment: {fb.comment}</p>
-                                <h3> Rating: {fb.rating}</h3>
-                                <p>Review added on: {new Date(fb.createdAt).toLocaleString()} </p>
-                                <p>Review last updated: {new Date(fb.updatedAt).toLocaleString()} </p> <br />
-                                {/* // edit */}
-                                <button>
-                                    <a href={`/feedback/${fb._id}/edit`}> Edit Review</a>
-                                </button>
-                                <form action={`/feedback/${fb._id}?_method=DELETE`} method="POST">
-                                    <input type="submit" value="Delete Review" />
-                                </form>
-                            </>
-                        })}
+                    <div class="review-container">
+                        <h3>
+                            <button class="add-review-btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+                                Add a Review
+                            </button>
+                        </h3>
+                        <div class="collapse collapse-horizontal" id="collapseWidthExample">
+                            <div class="card card-body" style={{ width: '30%', alignItems: 'center' }}>
+                                <div class="new-form">
+                                    <form method="POST" action="/feedback">
+                                        <div class="mt-2 mb-1 col-sm-13">
+                                            <label for="comment" class="form-label">  Comment: </label>
+                                            <input id="comment" class="form-control" name="comment" type="textarea" required />
+                                        </div>
+                                        <div class="mt-2 mb-1 col-sm-13">
+                                            <label for="rating" class="form-label"> Rating: </label>
+                                            <input id="rating" class="form-control" name="rating" type="number" min="0" step="1" max="5" required />
+                                        </div>
+                                        <label class="hidden" for="snackItem"> Item: </label>
+                                        <input class="hidden" type="snackItem" name="snackItem" value={oneSnackItem._id} required /> <br />
 
-                    </section>
+                                        <button class="add-review-btn" type="submit">
+                                            Submit
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <section >
+                            <h2>Reviews of {oneSnackItem.name}</h2>
+                            {feedbacks.map((fb, i) => {
+                                return <div>
+                                    <p> Comment: {fb.comment}</p>
+                                    <h3> Rating: {fb.rating}</h3>
+                                    <p>Review added on: {new Date(fb.createdAt).toLocaleString()} </p>
+                                    <p>Review last updated: {new Date(fb.updatedAt).toLocaleString()} </p> <br />
+                                    {/* // edit */}
+                                    <button>
+                                        <a href={`/feedback/${fb._id}/edit`}> Edit Review</a>
+                                    </button>
+                                    <form action={`/feedback/${fb._id}?_method=DELETE`} method="POST">
+                                        <input type="submit" value="Delete Review" />
+                                    </form>
+                                </div>
+                            })}
+
+                        </section>
+                    </div>
+
 
                 </div>
-            </DefaultLayout>
+            </DefaultLayout >
         );
     }
 }
